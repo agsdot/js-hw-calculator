9334d48b8a95cdbc580acc0ec2a70d703a8fbc7f
Jonathan Lai <jonathanlai@eltux.net>
2013-04-25 23:20:55 -0700

    4/25/13 2320pm save

--- js/calculator.js
+++ js/calculator.js
@@ -1,6 +1,6 @@
 
-function calculator(operator, operand1, operand2){
-	switch(operator) {
+function calculator(a, operand1, operand2){
+	switch(a) {
 		case "1":
 		return operand1 + operand2;
 		break;
@@ -22,9 +22,9 @@ function calculator(operator, operand1, operand2){
 
 
 
-	var operator = prompt("Please select which operation you want to do with a number: 1 for addition, 2 for subtraction, 3 for multiplication, 4 for division, and 5 for modulus");
-	var operand1 = parseInt( prompt("Give me the first number"), 10);
-	var operand2 = parseInt( prompt("Give me the second number"), 10);
+var operator = prompt("Please select which operation you want to do with a number: 1 for addition, 2 for subtraction, 3 for multiplication, 4 for division, and 5 for modulus");
+var operand1 = parseInt( prompt("Give me the first number"), 10);
+var operand2 = parseInt( prompt("Give me the second number"), 10);
 
 
 document.write(calculator(operator, operand1, operand2));
